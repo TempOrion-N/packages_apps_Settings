fcf7db424326eb2db0503ab0715e872f34e8f989
SohamJ <soham4jam@gmail.com>
2016-10-27 15:30:35 -0400

    Use the support switch preference

--- src/com/android/settings/slim/SystemSettingSwitchPreference.java
+++ src/com/android/settings/slim/SystemSettingSwitchPreference.java
@@ -18,7 +18,7 @@
 package com.android.settings.slim;
 
 import android.content.Context;
-import android.preference.SwitchPreference;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
